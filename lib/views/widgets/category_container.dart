97d6439350ae183dcc6fd123e6eb2f4a97983ba4
Henrique Souza <henrique.asouza@outlook.com>
2021-11-26 18:50:20 -0300

    refactor(category_container): component is now statefull to better suit the project needs

--- lib/views/widgets/category_container.dart
+++ lib/views/widgets/category_container.dart
@@ -1,44 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_modulo_1/controllers/categories_controller.dart';
 
-class CategoryContainer extends StatelessWidget {
-  final TextEditingController controller;
+class CategoryContainer extends StatefulWidget {
+  final CategoryController catController;
 
-  const CategoryContainer({Key? key, required this.controller})
+  const CategoryContainer({Key? key, required this.catController})
       : super(key: key);
+
+  @override
+  _CategoryContainerState createState() => _CategoryContainerState();
+}
+
+class _CategoryContainerState extends State<CategoryContainer> {
+  //CategoryController catController = CategoryController();
+  //int catId = 0;
+
+  void nextItem() {
+    setState(() {
+      widget.catController.selectedCat <
+              widget.catController.categories.length - 1
+          ? widget.catController.selectedCat++
+          : widget.catController.selectedCat = 0;
+    });
+  }
+
+  void previousItem() {
+    setState(() {
+      widget.catController.selectedCat--;
+      widget.catController.selectedCat > 0
+          ? widget.catController.selectedCat--
+          : widget.catController.selectedCat =
+              widget.catController.categories.length - 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Container(
+        SizedBox(
           height: 50,
           width: 50,
-          child: const Icon(
-            Icons.arrow_downward,
+          child: IconButton(
+            icon: const Icon(Icons.arrow_downward),
+            tooltip: 'Previous Category',
+            onPressed: () {
+              previousItem();
+            },
           ),
         ),
-        Container(
-          height: 50,
-          width: 125,
-          child: Column(
+        SizedBox(
+          height: 100,
+          width: 150,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextField(
-                controller: controller,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 20, // This is not so important
-                ),
-                enabled: false,
-              )
+              Image(
+                  image: widget.catController
+                      .categories[widget.catController.selectedCat].thumb),
+              const Spacer(),
+              Text(
+                  widget.catController
+                      .categories[widget.catController.selectedCat].name,
+                  style: const TextStyle(fontSize: 18))
             ],
           ),
         ),
-        Container(
+        SizedBox(
           height: 50,
           width: 50,
-          child: const Icon(
-            Icons.arrow_upward,
+          child: IconButton(
+            icon: const Icon(Icons.arrow_upward),
+            tooltip: 'Next Category',
+            onPressed: () {
+              nextItem();
+            },
           ),
         ),
       ],
